62e06cfc1462566046b38defe68160e244101302
KieuTranThuUyen <uyen893605@gmail.com>
2025-12-04 01:22:43 +0700

    Sửa NavDestination.kt

--- app/src/main/java/com/example/medinotify/ui/navigation/NavDestination.kt
+++ app/src/main/java/com/example/medinotify/ui/navigation/NavDestination.kt
@@ -1,2 +1,26 @@
 package com.example.medinotify.ui.navigation
 
+sealed class NavDestination(val route: String) {
+    // AUTH
+    object Splash : NavDestination("splash")
+    object Login : NavDestination("login")
+    object Register : NavDestination("register")
+    object ForgotPassword : NavDestination("forgot_password")
+    object VerifyCode : NavDestination("verify_code")
+    object ResetPassword : NavDestination("reset_password")
+    object ResetPasswordSuccess : NavDestination("reset_password_success")
+
+    // MAIN APP
+    object Home : NavDestination("home")
+    object Calendar : NavDestination("calendar")
+    object MedicineHistory : NavDestination("medicine_history")
+    object MedicineHistoryDetail : NavDestination("medicine_history_detail/{date}") {
+        fun createRoute(date: String) = "medicine_history_detail/$date"
+    }
+    object StartAddMedicine : NavDestination("start")
+    object AddMedicine : NavDestination("add")
+
+    companion object {
+        val startDestination = Splash.route
+    }
+}
